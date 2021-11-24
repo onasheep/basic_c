19a936ecc0dee04ced26927f0875b29c0fc47685
onasheep <inabass@naver.com>
2021-11-24 20:42:45 +0900

    first commit

---
+++ myProject/loop.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+	/*printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");*/
+
+	// ++»Ô»Ô
+	/*int a = 10;
+	printf("a´Â %d\n", a);
+	a++;
+	printf("a´Â %d\n", a);
+	a++;
+	printf("a´Â %d\n", a);*/
+	/*int b = 20;
+	printf("b´Â %d\n", ++b);
+	printf("b´Â %d\n", b++);
+	printf("b´Â %d\n", b);*/
+
+	int i = 1;
+	printf("Hello Wordl %d\n", i++);
+	printf("Hello Wordl %d\n", i++);
+	printf("Hello Wordl %d\n", i++);
+	printf("Hello Wordl %d\n", i++);
+	printf("Hello Wordl %d\n", i++);
+	printf("Hello Wordl %d\n", i++);
+	printf("Hello Wordl %d\n", i++);
+	printf("Hello Wordl %d\n", i++);
+	printf("Hello Wordl %d\n", i++);
+	printf("Hello Wordl %d\n", i++);
+	printf("Hello Wordl %d\n", i++);
+	printf("Hello Wordl %d\n", i++);
+	printf("Hello Wordl %d\n", i++);
+	printf("Hello Wordl %d\n", i++);
+	printf("Hello Wordl %d\n", i++);
+	
+	return 0;
+
+}
